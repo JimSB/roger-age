a2833af5c4609a63cf56c60c23bf1184aa7078ea
JimSB <jimsuleyman@live.com>
2014-11-09 22:17:13 -0500

    fist official commit

---
+++ WilliamAge.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class WilliamAge
+
+{
+    public static void main(String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+        
+        int age;
+        
+        System.out.print("How old is William?");
+        age = keyboard.nextInt();
+        
+        if (age < 2) 
+            System.out.println("NO! He's not that young!");
+        else
+        {
+            if (age > 2)
+                System.out.println("That's way too old!");
+            else
+            {
+                if (age == 2)
+                    System.out.println("Good, otherwise I'm programmed to tell you you're wrong!");
+            }
+        }
+    }
+}
+            
